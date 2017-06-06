503c47f79bdf272156e4fff9422d1d5ffa486c66
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-06-06 17:31:45 +0100

    commit garage task 2 end of day 06/06/2017

--- src/main/scala/Garage/Garage.scala
+++ src/main/scala/Garage/Garage.scala
@@ -1,8 +1,27 @@
 package Garage
 
-/**
-  * Created by Administrator on 06/06/2017.
-  */
-class Garage {
+import scala.collection.mutable.ArrayBuffer
 
+class Garage {
+  var personList = new ArrayBuffer[Person]()
+  var vehicleList = new ArrayBuffer[Vehicle]()
+
+
+  def spitPersonList(array: ArrayBuffer[Person]): Any = {
+    for (p <- array.toArray) println(p)
+  }
+  def spitVehicleList(array: ArrayBuffer[Vehicle]): Any = {
+    for (v <- array.toArray) println(v)
+  }
+
+  def addCar(brand: String, make: String, regNo: String, numberOfDoors: Int, numberOfGears: Int): Unit = {
+    vehicleList += new Car(brand, make, regNo, numberOfDoors, numberOfGears, false)
+  }
+  def addBike(brand: String, make: String, regNo: String, typeOfBike: String): Unit = {
+    vehicleList += new Bike(brand, make, regNo, typeOfBike, false)
+  }
+
+  def removeByType(vehicleType: String): Unit = {
+    vehicleList
+  }
 }
--- src/main/scala/Garage/Runner.scala
+++ src/main/scala/Garage/Runner.scala
@@ -5,33 +5,18 @@ import scala.collection.mutable.ArrayBuffer
 object Runner {
 
   def main(args: Array[String]): Unit = {
-    var personList = new ArrayBuffer[Person]()
-    var vehicleList = new ArrayBuffer[Vehicle]()
+    val garage = new Garage()
+
     val emp = new Employee(1, "John Smith", 25, "0800001066", "Cleaner", 9.87)
     val cus = new Customer(1, "Peter Out", 65, "01009998760", "42 Wallaby Way, Sydney", "SYD NEY")
-    val car = new Car("BMW", "X5", "4BCD 3FG", 4, 6, false)
-    val bik = new Bike("Spark", "", "", "Bicycle", true)
 
-    personList += emp
-    personList += cus
-    vehicleList += car
-    vehicleList += bik
-    spitPersonList(personList)
-    spitVehicleList(vehicleList)
-    println()
 
-    emp.employeeID = 0
-    emp.role = "Unemployed"
-    emp.wage = 0
-    spitPersonList(personList)
-    spitVehicleList(vehicleList)
+    garage.personList += emp
+    garage.personList += cus
+    garage.addCar("BMW", "X5", "R3GN UMB", 4, 6)
+    garage.addBike("Spark", null, null, "Bicycle")
+    garage.spitPersonList(garage.personList)
+    garage.spitVehicleList(garage.vehicleList)
   }
 
-  def spitPersonList(array: ArrayBuffer[Person]): Any = {
-    for (p <- array.toArray) println(p)
-  }
-
-  def spitVehicleList(array: ArrayBuffer[Vehicle]): Any = {
-    for (v <- array.toArray) println(v)
-  }
 }
