6eaa3983eb8315a827698197619a83e4b3a0f13e
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-06-08 11:22:07 +0100

    commit make vals lazy

--- src/main/scala/Garage/Runner.scala
+++ src/main/scala/Garage/Runner.scala
@@ -5,8 +5,8 @@ object Runner {
   def main(args: Array[String]): Unit = {
     val garage = new Garage()
 
-    val emp = new Employee(1, "John Smith", 25, "0800001066", "Cleaner", 9.87)
-    val cus = new Customer(1, "Peter Out", 65, "01009998760", "42 Wallaby Way, Sydney", "SYD NEY")
+    lazy val emp = new Employee(1, "John Smith", 25, "0800001066", "Cleaner", 9.87)
+    lazy val cus = new Customer(1, "Peter Out", 65, "01009998760", "42 Wallaby Way, Sydney", "SYD NEY")
 
     garage.personList += emp
     garage.personList += cus
