7327e889bfe3b5a70c6b550fd7b18b99318c5249
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-06-15 14:45:21 +0100

    commit curly boys

--- src/main/scala/Curls.scala
+++ src/main/scala/Curls.scala
@@ -6,7 +6,7 @@ object Curls extends App {
     if(curlyBoys == 1) println(s"Wait! Now there is $curlyBoys curly boy!")
     else {
       println(s"Now there are $curlyBoys curly boys!")
-      Thread.sleep(1000)
+      Thread.sleep(500)
     }
   }
 }
